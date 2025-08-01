f50386f7f0e195563f4e714462c79355499b4514
Welski Canlas <rccanlas2@gmail.com>
2025-08-01 22:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {"message": "Hello from Coolify!"}
+    # Change this message!
+    return {"message": "This is a new version deployed automatically!"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
