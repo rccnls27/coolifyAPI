94aef465e62fd8fa03c6753db63e0ca3869271cd
Welski Canlas <rccanlas2@gmail.com>
2025-08-09 10:37:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def add(a, b):
 
 # This part is only for local development, Coolify doesn't use it.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
